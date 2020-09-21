e9e6cb9bb2d7aec27962ef92bd3dbb38ad0040a0
Rustem81 <garantkredo@gmail.com>
2020-09-21 17:19:41 +0500

    Initial commit

---
+++ assets/js/page--src-pages-404-vue.215443f6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"7l0S":function(t,e,n){"use strict";n.r(e);var l=n("KHd+"),s=Object(l.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("Layout",[e("h1",{staticClass:"p-3 p-lg-5 d-flex align-items-center"},[this._v("404")])])}),[],!1,null,null,null);e.default=s.exports}}]);
